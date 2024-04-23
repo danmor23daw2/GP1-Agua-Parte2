7ea7cd7282cd6d72974793c747ebc70885734834
jossan23daw2 <15586411.clot@fje.edu>
2024-04-23 16:52:32 +0200

    Mongose Acabado Danise

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/alumnes', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 
-// view engine setup
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+const app = express();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -20,26 +28,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/pag1', indexRouter);
-app.use('/pag2', indexRouter);
-app.use('/pag3', indexRouter);
-app.use('/pag4', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-// Importa las clases
-const { Campo, Impacto } = require('./classes.js'); 
+let operacions = 0;
 
-/* GET home page. */
+const alumneSchema = new mongoose.Schema({
+    nom: String,
+    email: String
+});
+
+const Alumne = mongoose.model('Alumne', alumneSchema);
 
 router.get('/', function(req, res, next) {
   res.render('inicioSesion', { });
 });
+
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'GP1 Agua Parte 2' });
 });
+
 router.get('/pag1', function(req, res, next) {
   res.render('pag1', { });
 });
@@ -19,13 +26,10 @@ router.get('/pag1', function(req, res, next) {
 router.get('/pag2', function(req, res, next) {
   const campo = new Campo('Cancha de Futbol');
   const impacto = new Impacto('el medio ambiente');
-
   campo.gasto();
   impacto.gasto(); 
-
   res.render('pag2', { campo, impacto }); 
 });
-
 
 router.get('/pag3', function(req, res, next) {
   res.render('pag3', { title: 'Apoya a la causa'});
@@ -40,11 +44,78 @@ router.get('/modificar', function(req, res, next) {
 });
 
 router.get('/eliminar', function(req, res, next) {
-  res.render('eliminar', {title: 'Elimina Tu Nombre' });
+  res.render('eliminar', { title: 'Elimina Tu Nombre' });
 });
 
-router.get('/modificar', function(req, res, next) {
-  res.render('eliminar', { });
+//Mongoose ------------------------------------------------
+
+router.get('/llistarAlumnes', function(req, res) {
+    Alumne.find({})
+        .then(docs => {
+            console.log(docs);
+            res.render('llistarAlumnes', {
+                llistarAlumnes: docs,
+                operacions: operacions
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            res.send("problemes amb la base de dades.");
+        });
 });
+
+router.post('/afegirAlumneBD', function(req, res) {
+    var nom = req.body.nom;
+    var email = req.body.email;
+
+    console.log("Nom:", nom);
+    console.log("Email:", email);
+
+    const nouAlumne = new Alumne({
+        nom: nom,
+        email: email
+    });
+
+    nouAlumne.save()
+        .then(() => {
+            operacions++;
+            res.redirect("/llistarAlumnes");
+        })
+        .catch(err => {
+            console.error("Error al guardar:", err);
+            res.send("problemes amb la base de dades.");
+        });
+});
+
+router.post('/modificarAlumneBD', function(req, res) {
+    var nom = req.body.nom;
+    var email = req.body.email;
+
+    Alumne.findOneAndUpdate({ email: email }, { nom: nom })
+        .then(() => {
+            operacions++;
+            res.redirect("/llistarAlumnes");
+        })
+        .catch(err => {
+            console.error(err);
+            res.send("problemes amb la base de dades.");
+        });
+});
+
+router.post('/esborrarAlumneBD', function(req, res) {
+    var email = req.body.email;
+
+    Alumne.findOneAndDelete({ email: email })
+        .then(() => {
+            operacions++;
+            res.redirect("/llistarAlumnes");
+        })
+        .catch(err => {
+            console.error(err);
+            res.send("problemes amb la base de dades.");
+        });
+});
+
+//Mongoose -------------------------------------------------------
 
 module.exports = router;
