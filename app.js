4eed43ee55fdff904bcb1b19b31478fb232f4e6e
danmor23daw2 <15586175@fje.edu>
2024-04-29 19:45:50 +0200

    Https

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,11 +14,10 @@ const usersRouter = require('./routes/users');
 mongoose.connect('mongodb://127.0.0.1:27017/alumnes', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'error:'));
 db.once('open', function() {
-    console.log('Connected to MongoDB');
+    console.log('Conectado a MongoDB');
 });
-
 
 const app = express();
 
@@ -33,7 +34,7 @@ app.use(session({
     secret: 'UnaCadenaMuyLargaYDifícilDeAdivinar123456', 
     resave: false,
     saveUninitialized: false
-  }));
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -56,6 +57,17 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
+});
+
+const httpsCertificados = {
+    key: fs.readFileSync('./public/claves/GP1_privada.key'),
+    cert: fs.readFileSync('./public/claves/GP1.crt')
+};
+
+const server = https.createServer(httpsCertificados, app);
+
+server.listen(3001, () => {
+    console.log('Server running on https://localhost:3001/');
 });
 
 module.exports = app;
